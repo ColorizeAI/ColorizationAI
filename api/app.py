fa5cf38b6ac1a37afa9aba4ee8c0dec7a86af21f
AmineMousssaif <71696893+AmineMousssaif@users.noreply.github.com>
2023-11-25 16:37:16 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request, send_from_directory
 import cv2
 import numpy as np
-import os
+from io import BytesIO
 
 app = Flask(__name__)
-
-# Ensure the 'static/uploads' directory exists
-os.makedirs('static/uploads', exist_ok=True)
 
 @app.route('/')
 def index():
@@ -25,16 +22,17 @@ def upload_file():
     if file.filename == '':
         return render_template('index.html', error='No selected file')
 
-    # Save the uploaded file
-    file_path = 'static/uploads/input.jpg'
-    file.save(file_path)
+    # Read the uploaded file into memory
+    file_content = file.read()
+    file_data = BytesIO(file_content)
 
     # Implement the colorization code here
-    colorized_path = colorize_image(file_path)
+    colorized_image = colorize_image(file_data)
 
-    return render_template('result.html', input_image='input.jpg', colorized_image='colorized.jpg')
+    # Return the colorized image as a response
+    return render_template('result.html', input_image='input.jpg', colorized_image=colorized_image)
 
-def colorize_image(image_path):
+def colorize_image(file_data):
     prototxt_path = "models/colorization_deploy_v2.prototxt"
     model_path = "models/colorization_release_v2.caffemodel"
     kernel_path = "models/pts_in_hull.npy"
@@ -46,7 +44,9 @@ def colorize_image(image_path):
     net.getLayer(net.getLayerId('class8_ab')).blobs = [points.astype("float32")]
     net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, dtype="float32")]
 
-    image = cv2.imread(image_path)
+    # Decode the image directly from memory
+    image = cv2.imdecode(np.frombuffer(file_data.read(), np.uint8), cv2.IMREAD_COLOR)
+
     normalized = image.astype("float32") / 255.0
     lab = cv2.cvtColor(normalized, cv2.COLOR_BGR2LAB)
 
@@ -64,15 +64,11 @@ def colorize_image(image_path):
     colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
     colorized = (255.0 * colorized).astype("uint8")
 
-    # Save the colorized image
-    colorized_path = 'static/uploads/colorized.jpg'
-    cv2.imwrite(colorized_path, colorized)
-
-    return colorized_path
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory('static/uploads', filename)
+    # Encode the colorized image to JPEG format
+    _, colorized_data = cv2.imencode('.jpg', colorized)
+    
+    # Return the colorized image as bytes
+    return BytesIO(colorized_data).read()
 
 if __name__ == '__main__':
     app.run(debug=True)
