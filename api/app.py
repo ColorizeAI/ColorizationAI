9088ebaf10c17179cdeff913e183cb389b26a0ab
AmineMousssaif <71696893+AmineMousssaif@users.noreply.github.com>
2023-11-25 16:53:07 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -33,9 +33,9 @@ def upload_file():
     return render_template('result.html', input_image='input.jpg', colorized_image=colorized_image)
 
 def colorize_image(file_data):
-    prototxt_path = "models/colorization_deploy_v2.prototxt"
-    model_path = "models/colorization_release_v2.caffemodel"
-    kernel_path = "models/pts_in_hull.npy"
+    prototxt_path = "api/models/colorization_deploy_v2.prototxt"
+    model_path = "api/models/colorization_release_v2.caffemodel"
+    kernel_path = "api/models/pts_in_hull.npy"
 
     net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
     points = np.load(kernel_path)
